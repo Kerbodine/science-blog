2931b5366323c99e3575bbdf56c43ded1a3f7c86
Michael Tong <michaelyuyangtong@gmail.com>
2022-03-07 21:03:28 +0800

    Updated styles and favicon

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,31 @@ import path from "path";
 import matter from "gray-matter";
 import Post from "../components/Post";
 import { sortByDate } from "../utils";
+import Head from "next/head";
 
 export default function Home({ posts }) {
   console.log(posts);
 
   return (
-    <div className="mx-auto w-full max-w-4xl px-8 pt-16">
-      <h1 className="text-3xl font-semibold leading-8">All Articles:</h1>
-      <hr className="my-4 border-0 border-b-2 border-gray-200" />
-      {posts.map((post, index) => (
-        <Post key={index} post={post} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Physics Blog Site</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Physics ideas - Simply explained. Designed and written by Michael Tong"
+        />
+      </Head>
+      <div className="mx-auto w-full max-w-4xl px-8 pt-16">
+        <h1 className="text-3xl font-semibold leading-8">All Articles:</h1>
+        <hr className="my-4 border-0 border-b-2 border-gray-200" />
+        <div className="flex flex-col gap-2">
+          {posts.map((post, index) => (
+            <Post key={index} post={post} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
